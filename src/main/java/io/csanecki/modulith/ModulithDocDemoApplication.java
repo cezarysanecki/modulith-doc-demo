77e752aae3fe6d0b691c99ef0bdbbca6814c9c9a
Cezary Sanecki <devcezz@gmail.com>
2023-02-10 15:03:12 +0100

    Initial commit

---
+++ src/main/java/io/csanecki/modulith/ModulithDocDemoApplication.java
@@ -0,0 +1,13 @@
+package io.csanecki.modulith;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ModulithDocDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ModulithDocDemoApplication.class, args);
+    }
+
+}
